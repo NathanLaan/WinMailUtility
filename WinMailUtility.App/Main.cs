401f53e8460aac1b554a0b80d0f390376b7418fb
Nathan Laan <nathanlaan@gmail.com>
2013-03-12 11:12:08 -0600

    Initial project setup.

---
+++ WinMailUtility.App/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinMailUtility.App
+{
+    static class Main
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
